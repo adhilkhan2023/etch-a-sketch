e5222f5acdec7760e4b0379d7f5c683bd67e76de
Adhil Khan <adhilkhan2023@gmail.com>
2024-03-29 16:49:17 +0530

    feat: add a reset button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
 
+const resetButton = document.querySelector('.reset-button');
+
+let numberOfSquares;
+
 for(i = 1; i <= (16 * 16); i++){
     let squareDiv = document.createElement('div');
     squareDiv.style.width = `${800/16}px`;
@@ -12,3 +16,8 @@ for(i = 1; i <= (16 * 16); i++){
 
     container.appendChild(squareDiv);
 }
+
+resetButton.addEventListener('click',function(){
+    numberOfSquares = prompt(`Enter a number between 1 and 100.
+(The number you enter will take as the number of squares per side for the new grid.)`)
+})
