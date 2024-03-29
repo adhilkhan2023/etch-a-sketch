99e73300a0fba7a1fa4d6336821ddbd3a4621e4a
Adhil Khan <adhilkhan2023@gmail.com>
2024-03-29 21:13:33 +0530

    feat: allow user to create custom grid

--- script.js
+++ script.js
@@ -20,4 +20,29 @@ for(i = 1; i <= (16 * 16); i++){
 resetButton.addEventListener('click',function(){
     numberOfSquares = prompt(`Enter a number between 1 and 100.
 (The number you enter will take as the number of squares per side for the new grid.)`)
+
+    if(numberOfSquares < 1 || numberOfSquares > 100){
+        alert('invalid number!');
+    }else{
+        let childOfContainer = container.firstElementChild;
+
+        // remove every div in the container
+        while(childOfContainer){
+            container.removeChild(childOfContainer);
+            childOfContainer = container.firstElementChild;
+        }
+
+        for(i = 1; i <= (numberOfSquares * numberOfSquares); i++){
+            let squareDiv = document.createElement('div');
+            squareDiv.style.width = `${800/numberOfSquares}px`;
+            squareDiv.style.height = `${800/numberOfSquares}px`;
+            squareDiv.style.border = `0.1px solid black`;
+        
+            squareDiv.addEventListener('mouseenter',function(){
+                squareDiv.style.backgroundColor = 'black';
+            })
+        
+            container.appendChild(squareDiv);
+        }
+    }
 })
